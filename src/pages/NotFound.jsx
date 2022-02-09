fe27002d85b980cd3ade876eb5a5462a808e2d7e
Lucas Avila <lucasg.avilas@gmail.com>
2022-02-09 01:34:21 -0300

    Proyecto sobre pagina de compras

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/NotFound.scss'
+
+const NotFound = () => {
+    return (
+        <div >
+            <img src="https://josefacchin.com/wp-content/uploads/2018/09/http-not-found-error-404.png" 
+            className='image' alt="" />
+        </div>
+    );
+}
+
+export default NotFound;
